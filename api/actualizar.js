adca5d8e63bd1e91fc92612e9e90e3c8f8c30e01
neomargh-pixel <neomargh@gmail.com>
2026-07-31 23:27:35 -0400

    Update actualizar.js

--- api/actualizar.js
+++ api/actualizar.js
@@ -1,15 +1,25 @@
+import * as cheerio from "cheerio";
+
 export default async function handler(req, res) {
 
 try {
 
 const respuesta = await fetch("https://www.tuazar.com/loteria/animalitos/resultados/");
-
 const html = await respuesta.text();
 
-res.status(200).json({
-ok:true,
-tamano: html.length
+const $ = cheerio.load(html);
+
+let resultados = [];
+
+$(".resultado, .result, .item").each((i, el) => {
+
+resultados.push({
+texto: $(el).text().trim()
 });
+
+});
+
+res.status(200).json(resultados);
 
 } catch(error){
 
