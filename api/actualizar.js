fdfed858d73b8a21b6ba0948de47af7efc75154a
neomargh-pixel <neomargh@gmail.com>
2026-07-31 23:13:07 -0400

    Update actualizar.js

--- api/actualizar.js
+++ api/actualizar.js
@@ -1,1 +1,24 @@
+const axios = require("axios");
 
+export default async function handler(req, res) {
+
+try {
+
+const pagina = await axios.get("https://www.tuazar.com/loteria/animalitos/resultados/");
+
+res.status(200).json({
+ok:true,
+mensaje:"Conexión realizada correctamente.",
+tamano: pagina.data.length
+});
+
+} catch(error){
+
+res.status(500).json({
+ok:false,
+error:error.message
+});
+
+}
+
+}
